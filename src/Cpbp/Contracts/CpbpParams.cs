b3510a5183acd7294c8742165763a03f593ce66e
Mehmet Burak Muştu <47041552+brkmustu@users.noreply.github.com>
2020-01-17 21:29:23 +0300

    Delete CpbpParams.cs
    because it has been renamed and moved to the home directory

--- src/Cpbp/Contracts/CpbpParams.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cpbp.Contracts
-{
-    public class CpbpParams
-    {
-        public static string[] Args { get; internal set; }
-        public static string Argument { get; internal set; }
-        public static string Value { get; internal set; }
-    }
-}
